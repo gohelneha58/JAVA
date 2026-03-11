1e54920fd6e550d7c0f79c354f79e7bd42bdcdd1
gohel neha <gohelneha58@gmail.com>
2026-03-11 10:51:32 +0530

    data uploaded

---
+++ neha_os/shell.sh
@@ -0,0 +1,18 @@
+case $age in
+18) echo "Eighteen";;
+21) echo "Twenty one";;
+*) echo "Not found";;
+esac
+
+echo "position zero" = $0;
+echo "position one" = $1;
+echo "position two" = $2;
+echo "position three" = $3;
+echo "position four" = $4;
+echo "position five" = $5;
+echo "position six" = $6;
+echo "position seven" = $7;
+echo "position eight" = $8;
+echo "position nine" = $9;
+echo "position ten" = $10;
+
